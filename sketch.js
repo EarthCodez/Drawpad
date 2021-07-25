9b513bb7c1d54599a3c0df42054bf76a08aeb441
Paramesh S <71922456+RealParamesh@users.noreply.github.com>
2021-07-25 23:35:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,48 @@
 var sslide;
-var rslide;
-var gslide;
-var bslide;
-var cbutton;
-var dbutton;
+var rslide,bslide,gslide;
+var cbutton,ebutton,dbutton,fbutton,gbutton,hbutton;
+var cob1,cob2,cob3;
 var state="nod";
 var ms;
 var ssr;
-
+var c1,c2,c3;
 function setup() {
-  createCanvas(windowWidth-10,windowHeight-65);
-  sslide= createSlider(0,200,0);
+  createCanvas(windowWidth-200,windowHeight);
+//slider
   rslide= createSlider(0,255,0);
   gslide= createSlider(0,255,0);
   bslide= createSlider(0,255,0);
+  rslide.position(width,86);
+  bslide.position(width,108);
+  gslide.position(width,130);
+  sslide= createSlider(0,200,0);
+  sslide.position(width,0);
+  rslide.hide();
+  gslide.hide();
+  bslide.hide();
+  //button
+  fbutton= createButton("color settings");
+  ebutton= createButton("color");
+  gbutton=createButton("savecolor");
+  hbutton=createButton("choosecolor");
   cbutton= createButton("clear");
   dbutton= createButton("download your pic");
-  ms=createInput("circle");
+  ebutton.position(width,86);
+  gbutton.position(width,108);
+  hbutton.position(width,130);
+  fbutton.position(width,20);
+  cbutton.position(width,42);
+  dbutton.position(width,64);
+  // ebutton.hide();
+  // gbutton.hide();
+  // hbutton.hide();
+  //dropdowns
+  ms=createSelect();
+  ms.option("circle");
+  ms.option("square");
+  ms.option("triangle");
   background("black");
+  ms.position(width,152);
  //createP("(size rgb ) and r g b won't be captured in pic it will appear the fill selected & my brand name(not selling anything) , for comments message me at parameshgaming30@gmail.com")
 }
 
@@ -25,6 +50,7 @@ function draw() {
   textSize(20);
   noStroke();
   fill("white");
+  rectMode(CENTER);
   if(mouseIsPressed){
     fill(rslide.value(),gslide.value(),bslide.value());
     if(ms.value()=="circle"){
@@ -36,7 +62,7 @@ function draw() {
     else if(ms.value()=="triangle"){
       triangle(mouseX-100, mouseY, mouseX+100, mouseY, mouseX, mouseY-150)
     }
-    rect(0,0,100,100);
+    rect(50,50,100,100);
   } 
   if(mouseIsPressed){
     state="d";
@@ -49,11 +75,22 @@ function draw() {
      text("r",10,30);  
      text("g",10,45); 
      text("b",10,70); 
-     text("size,rgb",20,height-5);
 }
 ssr=sslide.value()
   cbutton.mousePressed(cllear);
   dbutton.mousePressed(download);
+  ebutton.mousePressed(godo);
+  // if(frameCount%2000==0){
+  //   rslide.hide();
+  //   gslide.hide();
+  //   bslide.hide();
+  //   ebutton.hide();
+  // gbutton.hide();
+  // hbutton.hide();
+  // }
+  fbutton.mousePressed(godoo);
+  gbutton.mousePressed(lesa);
+  hbutton.mousePressed(lech);
 }
 function cllear(){
   fill('black');
@@ -69,7 +106,7 @@ function download(){
   state="d";
   if(state=="d"){
   fill(rslide.value(),gslide.value(),bslide.value());
-  rect(0,0,100,100);
+  rect(50,50,100,100);
   rect(20,height-20,70,20);
     fill("white");
     textSize(10);
@@ -81,4 +118,36 @@ function download(){
 
 function mouseReleased(){
     (state="nod");
+}
+
+function godo(){
+  ebutton.hide();
+  gbutton.hide();
+  hbutton.hide();
+  rslide.show();
+  gslide.show();
+  bslide.show();
+  
+}
+
+function godoo(){
+  rslide.hide();
+  gslide.hide();
+  bslide.hide();
+  ebutton.show();
+  gbutton.show();
+  hbutton.show();
+}
+
+function lesa(){
+  c1=rslide.value();
+  c2=gslide.value();
+  c3=bslide.value();
+  console.log(sacol);
+}
+
+function lech(){
+   rslide.value(c1);
+   gslide.value(c2);
+   bslide.value(c3);
 }
